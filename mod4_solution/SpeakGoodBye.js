a4d6cfadaf7e6594ee18b377dc369646802a293f
marco marrone <marco marrone>
2016-08-09 09:25:02 +0200

    mod4 sol

---
+++ mod4_solution/SpeakGoodBye.js
@@ -0,0 +1,13 @@
+(function(window){
+
+	var speakWord = "Good Bye";
+
+	var byeSpeaker = {};
+	byeSpeaker.speak = function(name){
+		console.log(speakWord + " " + name);
+		};
+
+
+	window.byeSpeaker = byeSpeaker;
+
+})(window)
---
+++ mod4_solution/script.js
@@ -0,0 +1,15 @@
+(function (){
+
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (var name in names) {
+    var firstLetter = names[name].charAt(0);
+
+    if (firstLetter.toLowerCase() == "j") {
+        byeSpeaker.speak(names[name]);
+      }
+    else {
+        helloSpeaker.speak(names[name]);
+      }
+  };
+})()
+
